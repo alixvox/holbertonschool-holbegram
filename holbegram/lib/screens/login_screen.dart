384583259345cf4e1fa7b4b8ab39b5e7c6ce64cf
alixvox <alixvox@gmail.com>
2023-12-09 16:14:15 -0600

    Add LoginScreen and TextFieldInput widgets

---
+++ holbegram/lib/screens/login_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../widgets/text_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final bool passwordVisible;
+
+  LoginScreen({
+    required this.emailController,
+    required this.passwordController,
+    this.passwordVisible = true,
+  });
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late bool _passwordVisible;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = widget.passwordVisible;
+  }
+
+  @override
+  void dispose() {
+    widget.emailController.dispose();
+    widget.passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 28),
+            Text(
+              'Holbegram',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Billabong',
+                fontSize: 50,
+              ),
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 80,
+              height: 60,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 28),
+                  TextFieldInput(
+                    controller: widget.emailController,
+                    ispassword: false,
+                    hintText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: widget.passwordController,
+                    ispassword: !_passwordVisible,
+                    hintText: 'Password',
+                    keyboardType: TextInputType.visiblePassword,
+                    suffixIcon: IconButton(
+                      alignment: Alignment.bottomLeft,
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
